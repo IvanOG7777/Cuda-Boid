9a4061ab689005e6dd617ee3e0dd8a1cbbd2b8cd
Ivan <elderivanargueta04@gmail.com>
2026-08-12 18:45:50 -0700

    Fina do this shit tmrw

--- Tiled/main.cu
+++ Tiled/main.cu
@@ -1,3 +1,11 @@
 //
 // Created by elder on 8/12/2026.
 //
+
+#include <iostream>
+#include "deviceFunctions.cuh"
+
+int main() {
+    std:: cout << "Hello from tiled main" << std:: endl;
+    return 0;
+}
